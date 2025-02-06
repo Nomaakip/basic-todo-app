90269004c678bc64c3e6da107ffe85b2365ded24
hacks.guide <128547338+Nomaakip@users.noreply.github.com>
2025-02-06 20:45:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,139 @@
 function createItem() {
 
+
+
     const desc = document.getElementById('todo').value;
+
+
 
     if (desc) {
 
+
+
         const task = {
+
+
 
             desc
 
+
+
         }
+
+
 
         let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
+
+
         tasks.push(task);
+
+
 
         localStorage.setItem("tasks", JSON.stringify(tasks));
 
+
+
     }
+
+
 
     loadItem();
 
+
+
 }
+
+
+
+
 
 
 
 function loadItem() {
 
+
+
     const tasksContainer = document.getElementById('taskdiv');
+
+
 
     tasksContainer.innerHTML = "";
 
 
+
+
+
+
+
     const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+
 
     tasks.forEach((task, index) => {
 
+
+
         const taskElement = document.createElement('div');
+
+
 
         const tasksText = `<div id="atask">TODO
 
+
+
        <p style="font-size:15px">${task.desc}</p>
 
+
+
        <br>
+
        
+
        <button id ="remove" onclick="removeTask(${index})">remove</button>
+
+
 
        </div>`;
 
+
+
        taskElement.innerHTML = tasksText;
+
+
 
         tasksContainer.appendChild(taskElement);
 
+
+
     });
+
+
 
 }
 
+
+
 function removeTask(index) {
+
        let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+
 
         tasks.splice(index, 1);
 
+
+
         localStorage.setItem("tasks", JSON.stringify(tasks));
+
   
+
   loadItem();
+
 }
+
+
+
 
 
 loadItem();
